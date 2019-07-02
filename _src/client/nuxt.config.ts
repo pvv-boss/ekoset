36b1d3ef2b592f7ee2df8351fe7b42d18caa59f8
Ryzhkov76 <SergeyRyzhkov76@gmail.com>
2019-07-02 17:28:02 +0300

    Nuxt config

--- _src/client/nuxt.config.ts
+++ _src/client/nuxt.config.ts
@@ -3,8 +3,41 @@ import NuxtConfiguration from '@nuxt/config'
 const config: NuxtConfiguration = {
 
   mode: 'universal',
-
   srcDir: 'src/',
+
+  head: {
+    title: 'Экосеть',
+
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'Description', content: 'Экосеть' },
+      { name: 'Keywords', content: 'Экосеть' },
+      { name: 'theme-color', content: '#317EFB' },
+      { hid: 'og:title', property: 'og:title', content: 'Экосеть' },
+      { hid: 'og:image', property: 'og:image', content: 'Экосеть' },
+      { hid: 'og:description', property: 'og:description', content: 'Экосеть' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:image:secure_url', property: 'og:image:secure_url', content: 'Экосеть' },
+      { hid: 'og:image:type', property: 'og:image:type', content: 'image/png' },
+      { hid: 'og:image:width', property: 'og:image:width', content: '400' },
+      { hid: 'og:image:height', property: 'og:image:height', content: '300' },
+      { hid: 'og:image:alt', property: 'og:image:alt', content: 'Экосеть' }
+    ],
+
+    script: [
+      { src: 'https://unpkg.com/vue/dist/vue.runtime.min.js' },
+      { src: 'https://unpkg.com/vue-router/dist/vue-router.min.js' },
+      { src: 'https://unpkg.com/vuex/dist/vuex.min.js' },
+      { src: 'https://unpkg.com/axios/dist/axios.min.js' }
+    ],
+
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,500,700&&display=swap&subset=cyrillic' },
+      { rel: 'stylesheet', href: '/_nuxt/app.css' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+  },
 
   plugins: [
     {
@@ -38,8 +71,7 @@ const config: NuxtConfiguration = {
           'vue': 'Vue',
           'vuex': 'Vuex',
           'axios': 'axios',
-          'vue-router': 'VueRouter',
-          'element-ui': 'ELEMENT'
+          'vue-router': 'VueRouter'
         }
       }
       return config
