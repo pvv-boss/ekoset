fc492eb101e9b0c95f0f76d0784e5d2c7a3d5f2e
Maria Osokina <info@mariaosokina.ru>
2019-07-22 17:37:55 +0300

    pagination

---
+++ _src/client/src/components/ekoset/partials/Complain.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Пожаловаться</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class Complain extends Vue {
+
+}
+</script>
---
+++ _src/client/src/components/ekoset/partials/PersonalOffer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Персональное предложение</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class PersonalOffer extends Vue {
+
+}
+</script>
