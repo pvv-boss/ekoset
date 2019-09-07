3274afdb129b19367664eb0eccd4b8dbda198047
Ryzhkov76 <SergeyRyzhkov76@gmail.com>
2019-09-07 09:51:45 +0300

    dd

--- _src/client/src/middleware/baseMiddleware.ts
+++ _src/client/src/middleware/baseMiddleware.ts
@@ -4,10 +4,6 @@ import { NuxtContext } from 'vue/types/options';
 
 const baseMiddleware = (context: NuxtContext) => {
   const newSiteSection = context.params.siteSection ? context.params.siteSection : null
-
-  // tslint:disable-next-line:no-console
-  console.log('routemiddleware ' + newSiteSection)
-
   appStore(context.store).changeCurrentSiteSection(newSiteSection)
 }
 
--- _src/client/src/plugins/brc-route-middleware.ts
+++ _src/client/src/plugins/brc-route-middleware.ts
@@ -4,9 +4,5 @@ import { NuxtContext } from 'vue/types/options'
 
 export default (context: NuxtContext) => {
   const newSiteSection = context.params.siteSection ? context.params.siteSection : null
-
-  // tslint:disable-next-line:no-console
-  console.log('Plugin ' + newSiteSection)
-
   getModule(AppStore, context.store).changeCurrentSiteSection(newSiteSection)
 }
