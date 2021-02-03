355d0ec963276ed3fd13bd30b577282dd902fa1f
Ryzhkov76 <SergeyRyzhkov76@gmail.com>
2021-02-03 17:09:27 +0300

    ddd

--- _src/client/src/api/core/FetchRequest.ts
+++ _src/client/src/api/core/FetchRequest.ts
@@ -1,37 +1,35 @@
-import { RequestAPI, ResponseType } from './RequestAPI';
-import { ApiResponse } from './ApiResponse';
+import { RequestAPI, ResponseType } from "./RequestAPI";
+import { ApiResponse } from "./ApiResponse";
 
 // FIXME: Надо базовые настройки в конструктор как у аксиоса
 export default class FetchRequest extends RequestAPI {
+    protected async processRequest(url: string, responseType: ResponseType, config?: any, data?: any): Promise<ApiResponse> {
+        const reqData =
+            !!data && data instanceof FormData ? data : !!data ? (Object(data) === data ? JSON.stringify(data) : data) : null;
 
-  protected async processRequest (url: string, responseType: ResponseType, config?: any, data?: any): Promise<ApiResponse> {
-
-    const reqData = !!data && data instanceof FormData ? data : (!!data ? (Object(data) === data ? JSON.stringify(data) : data) : null)
-
-    if (!!reqData && !(data instanceof FormData)) {
-      config.headers = {
-        ...config.headers, ...{
-          'Content-Type': 'application/json'
+        if (!!reqData && !(data instanceof FormData)) {
+            config.headers = {
+                ...config.headers,
+                ...{
+                    "Content-Type": "application/json",
+                },
+            };
         }
-      }
-    }
 
-    const extConfig = { ...config, ...{ body: reqData } };
-    let response: ApiResponse;
+        const extConfig = { ...config, ...{ body: reqData } };
+        let response: ApiResponse;
 
-    try {
-      const result = await fetch(url, extConfig);
-      response = this.createResponse(result);
-      response.data = responseType === ResponseType.JSON ? await result.json() : await result.text();
-      if (!result.ok || result.status > 399) {
-        return Promise.reject(this.createErrorResponse({ response }));
-      }
+        try {
+            const result = await fetch(url, extConfig);
+            response = this.createResponse(result);
+            response.data = responseType === ResponseType.JSON ? await result.json() : await result.text();
+            if (!result.ok || result.status > 399) {
+                return Promise.reject(this.createErrorResponse({ response }));
+            }
 
-      return response;
+            return response;
+        } catch (err) {
+            return Promise.reject(this.createErrorResponse(err));
+        }
     }
-    catch (err) {
-      return Promise.reject(this.createErrorResponse(err));
-    }
-  }
 }
-
--- _src/client/src/api/core/RequestAPI.ts
+++ _src/client/src/api/core/RequestAPI.ts
@@ -1,12 +1,12 @@
-﻿import { ApiResponse } from './ApiResponse';
-import { Pagination } from '@/models/core/Pagination';
-import { Sort } from '@/models/core/Sort';
-import { RequestInterceptor, ResponseInterceptor } from './ApiInterceptor';
+﻿import { ApiResponse } from "./ApiResponse";
+import { Pagination } from "@/models/core/Pagination";
+import { Sort } from "@/models/core/Sort";
+import { RequestInterceptor, ResponseInterceptor } from "./ApiInterceptor";
 
 export enum ResponseType {
-  JSON,
-  TEXT,
-  ANY
+    JSON,
+    TEXT,
+    ANY,
 }
 
 // if (error.response) {
@@ -21,128 +21,124 @@ export enum ResponseType {
 // }
 // Сделать типизированные ошибки (сереверная, нет ответа, своя)
 
-
 export abstract class RequestAPI {
-  private _baseUrl?: string
-  private requestInterceptorList: RequestInterceptor[] = []
-  private responseInterceptorList: ResponseInterceptor[] = []
+    private _baseUrl?: string;
+    private requestInterceptorList: RequestInterceptor[] = [];
+    private responseInterceptorList: ResponseInterceptor[] = [];
 
-  constructor (options?: any, baseUrl?: string) {
-    this._baseUrl = baseUrl
-  }
-
-  public addRequestInterceptor (interceptor: RequestInterceptor) {
-    this.requestInterceptorList.push(interceptor);
-    return this.requestInterceptorList;
-  }
-
-  public addResponseInterceptor (interceptor: ResponseInterceptor) {
-    this.responseInterceptorList.push(interceptor);
-    return this.responseInterceptorList;
-  }
-
-  public set baseUrl (baseUrl: string | undefined) {
-    this._baseUrl = baseUrl;
-  }
-
-  public get baseUrl (): string | undefined {
-    return this._baseUrl;
-  }
-
-  public async getJSON (url: string, config?: any, pagination?: Pagination, sort?: Sort): Promise<ApiResponse> {
-    return this.doRequest(this.buildUrl(url, pagination, sort), ResponseType.JSON, { ...config, ...{ method: 'GET' } });
-  }
-
-  public async getText (url: string, config?: any, pagination?: Pagination, sort?: Sort): Promise<ApiResponse> {
-    return this.doRequest(this.buildUrl(url, pagination, sort), ResponseType.TEXT, { ...config, ...{ method: 'GET' } });
-  }
-
-  public async post (url: string, config?: any, data?: any): Promise<ApiResponse> {
-    return this.doRequest(this.buildUrl(url), ResponseType.JSON, { ...config, ...{ method: 'POST' } }, data);
-  }
-
-  public async postForm (url: string, config?: any, formData?: FormData): Promise<ApiResponse> {
-    config.headers = {
-      ...config.headers, ...{
-        'Content-Type': 'multipart/form-data'
-      }
+    constructor(options?: any, baseUrl?: string) {
+        this._baseUrl = baseUrl;
     }
 
-    return this.post(url, { ...config }, formData);
-  }
+    public addRequestInterceptor(interceptor: RequestInterceptor) {
+        this.requestInterceptorList.push(interceptor);
+        return this.requestInterceptorList;
+    }
 
-  public async put (url: string, config?: any, data?: any): Promise<ApiResponse> {
-    return this.doRequest(this.buildUrl(url), ResponseType.JSON, { ...config, ...{ method: 'PUT', data } }, data);
-  }
+    public addResponseInterceptor(interceptor: ResponseInterceptor) {
+        this.responseInterceptorList.push(interceptor);
+        return this.responseInterceptorList;
+    }
 
-  public async patch (url: string, config?: any, data?: any): Promise<ApiResponse> {
-    return this.doRequest(this.buildUrl(url), ResponseType.JSON, { ...config, ...{ method: 'PATCH', data } }, data);
-  }
+    public set baseUrl(baseUrl: string | undefined) {
+        this._baseUrl = baseUrl;
+    }
 
-  public async delete (url: string, config?: any): Promise<ApiResponse> {
-    return this.doRequest(this.buildUrl(url), ResponseType.JSON, { ...config, ...{ method: 'DELETE' } });
-  }
+    public get baseUrl(): string | undefined {
+        return this._baseUrl;
+    }
 
-  protected async doRequest (url: string, responseType: ResponseType, config?: any, data?: any): Promise<ApiResponse> {
-    const extConfig = this.beforProcessRequest(config);
-    const resp = await this.processRequest(url, responseType, extConfig, data)
-    return this.afterProcessResponse(resp)
-  }
+    public async getJSON(url: string, config?: any, pagination?: Pagination, sort?: Sort): Promise<ApiResponse> {
+        return this.doRequest(this.buildUrl(url, pagination, sort), ResponseType.JSON, { ...config, ...{ method: "GET" } });
+    }
 
-  protected beforProcessRequest (config: any) {
-    let extConfig = { ...config }
-    if (!!this.requestInterceptorList) {
-      for (const iter of this.requestInterceptorList) {
-        const addOptions = iter(extConfig);
-        if (!!addOptions) {
-          extConfig = { ...extConfig, addOptions }
+    public async getText(url: string, config?: any, pagination?: Pagination, sort?: Sort): Promise<ApiResponse> {
+        return this.doRequest(this.buildUrl(url, pagination, sort), ResponseType.TEXT, { ...config, ...{ method: "GET" } });
+    }
+
+    public async post(url: string, config?: any, data?: any): Promise<ApiResponse> {
+        return this.doRequest(this.buildUrl(url), ResponseType.JSON, { ...config, ...{ method: "POST" } }, data);
+    }
+
+    public async postForm(url: string, config?: any, formData?: FormData): Promise<ApiResponse> {
+        config.headers = {
+            ...config.headers,
+            ...{
+                "Content-Type": "multipart/form-data",
+            },
+        };
+
+        return this.post(url, { ...config }, formData);
+    }
+
+    public async put(url: string, config?: any, data?: any): Promise<ApiResponse> {
+        return this.doRequest(this.buildUrl(url), ResponseType.JSON, { ...config, ...{ method: "PUT", data } }, data);
+    }
+
+    public async patch(url: string, config?: any, data?: any): Promise<ApiResponse> {
+        return this.doRequest(this.buildUrl(url), ResponseType.JSON, { ...config, ...{ method: "PATCH", data } }, data);
+    }
+
+    public async delete(url: string, config?: any): Promise<ApiResponse> {
+        return this.doRequest(this.buildUrl(url), ResponseType.JSON, { ...config, ...{ method: "DELETE" } });
+    }
+
+    protected async doRequest(url: string, responseType: ResponseType, config?: any, data?: any): Promise<ApiResponse> {
+        const extConfig = this.beforProcessRequest(config);
+        const resp = await this.processRequest(url, responseType, extConfig, data);
+        return this.afterProcessResponse(resp);
+    }
+
+    protected beforProcessRequest(config: any) {
+        let extConfig = { ...config };
+        if (!!this.requestInterceptorList) {
+            for (const iter of this.requestInterceptorList) {
+                const addOptions = iter(extConfig);
+                if (!!addOptions) {
+                    extConfig = { ...extConfig, addOptions };
+                }
+            }
         }
-      }
+        return extConfig;
     }
-    return extConfig;
-  }
 
-  protected abstract processRequest (url: string, responseType: ResponseType, config?: any, data?: any): Promise<ApiResponse>
+    protected abstract processRequest(url: string, responseType: ResponseType, config?: any, data?: any): Promise<ApiResponse>;
 
-  protected afterProcessResponse (resp: ApiResponse) {
-    if (!!this.responseInterceptorList) {
-      for (const iter of this.responseInterceptorList) {
-        iter(resp);
-      };
+    protected afterProcessResponse(resp: ApiResponse) {
+        if (!!this.responseInterceptorList) {
+            for (const iter of this.responseInterceptorList) {
+                iter(resp);
+            }
+        }
+        return resp;
     }
-    return resp;
-  }
 
-  protected createResponse (resp: any): ApiResponse {
-    return {
-      data: resp.data,
-      status: resp.status,
-      statusText: resp.statusText,
-      headers: resp.headers
+    protected createResponse(resp: any): ApiResponse {
+        return {
+            data: resp.data,
+            status: resp.status,
+            statusText: resp.statusText,
+            headers: resp.headers,
+        };
     }
-  }
 
-  protected createErrorResponse (error: any): ApiResponse {
-    return this.createResponse(
-      !!error.response
-        ? error.response
-        : { data: null, status: 500, statusText: error.message }
-    )
-  }
-
-  //FIXME: тут надо с? решить 1 = 1
-  // page может быть и без офсета может в самом пагинации сделать формирование и в сорте
-
-  // или вообще как функцию передавать для форматирования урла извне
-  protected buildUrl (query: string, pagination?: Pagination, sort?: Sort): string {
-    let url = this._baseUrl ? `${this._baseUrl}/${query}` : query;
-    if (!!sort && !!sort.sortField) {
-      url = url + `&sortfield=${sort.sortField}&sorttype=${sort.sortType || 'DESC'}`;
+    protected createErrorResponse(error: any): ApiResponse {
+        return this.createResponse(!!error.response ? error.response : { data: null, status: 500, statusText: error.message });
     }
-    if (!!pagination) {
-      pagination.limit = pagination.limit > 0 ? pagination.limit : 10;
-      url = url + `&offset=${(pagination.currentPage - 1) * pagination.limit || 0}&limit=${pagination.limit}`;
+
+    //FIXME: тут надо с? решить 1 = 1
+    // page может быть и без офсета может в самом пагинации сделать формирование и в сорте
+
+    // или вообще как функцию передавать для форматирования урла извне
+    protected buildUrl(query: string, pagination?: Pagination, sort?: Sort): string {
+        let url = this._baseUrl ? `${this._baseUrl}/${query}` : query;
+        if (!!sort && !!sort.sortField) {
+            url = url + `&sortfield=${sort.sortField}&sorttype=${sort.sortType || "DESC"}`;
+        }
+        if (!!pagination) {
+            pagination.limit = pagination.limit > 0 ? pagination.limit : 10;
+            url = url + `&offset=${(pagination.currentPage - 1) * pagination.limit || 0}&limit=${pagination.limit}`;
+        }
+        return url;
     }
-    return url;
-  }
 }
